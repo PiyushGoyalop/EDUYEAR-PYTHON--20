f24ed45381fdd9f36f9d1711947ae4e52f934f5b
PiyushGoyalop <84716518+PiyushGoyalop@users.noreply.github.com>
2021-05-27 01:55:55 +0530

    Add files via upload

---
+++ Day 4_Assignment.py
@@ -0,0 +1,16 @@
+#OCCURENCE OF Y
+a="Franky is a 20 years old boy"
+b="y"
+print('The Number OF Times "Y" Occured is:',a.count(b))
+print("")
+
+#EVEN INDEXED CHARACTERS
+c="HI THERE, HOW ARE YOU?"
+print("The Even Indexed Characters Are:")
+print(c [: :2])
+print("")
+
+#SWAPCASE
+d="EdUyEaR"
+print("The Swapped String is:")
+print(d.swapcase())
